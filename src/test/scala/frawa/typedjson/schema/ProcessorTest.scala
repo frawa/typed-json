193bbff4b2d607c320c31a648b664b7647f49424
Frank Wagner <agilecoderfrank@gmail.com>
2021-07-24 22:43:44 +0200

    try processor

--- src/test/scala/frawa/typedjson/schema/ProcessorTest.scala
+++ src/test/scala/frawa/typedjson/schema/ProcessorTest.scala
@@ -1,9 +1,172 @@
 package frawa.typedjson.schema
 
 import munit.FunSuite
+import frawa.typedjson.parser.Value
+import frawa.typedjson.parser.ZioParser
+import frawa.typedjson.parser.Parser
+import frawa.typedjson.parser.NumberValue
+import frawa.typedjson.parser.BoolValue
+import frawa.typedjson.parser.NullValue
+import frawa.typedjson.parser.StringValue
+import frawa.typedjson.parser.ArrayValue
+import frawa.typedjson.parser.ObjectValue
+import scala.reflect.internal.Reporter
 
 class ProcessorTest extends FunSuite {
+  implicit val zioParser = new ZioParser();
 
-//   test("first") {}
+  private def withParsedSchemaValue(text: String, parse: String => Either[String, SchemaValue])(
+      f: SchemaValue => Unit
+  ) {
+    val withSchema = for {
+      value <- parse(text)
+    } yield {
+      f(value)
+    }
+    withSchema.swap
+      .map(message => fail("no schema", clues(clue(message))))
+      .swap
+  }
 
+  private def withSchema(text: String)(f: SchemaValue => Unit) {
+    withParsedSchemaValue(text, SchemaValue.apply)(f)
+  }
+
+  private def assertValidate(text: String)(schema: SchemaValue)(
+      f: ValidationResult => Unit
+  ) = {
+    val withParsed = for {
+      value <- Parser(text)
+      result = Processor.process(new ValidationCalculator())(schema, value)
+    } yield {
+      f(result)
+    }
+    withParsed.swap
+      .map(message => fail("parsing failed", clues(clue(message))))
+      .swap
+  }
+
+  test("null") {
+    withSchema("""{"type": "null"}""") { schema =>
+      assertValidate("""null""")(schema) { result =>
+        assertEquals(result.errors, Seq())
+        assertEquals(result.valid, true)
+      }
+      assertValidate("""13""")(schema) { result =>
+        assertEquals(result.errors, Seq(WithPointer(TypeMismatch2("null"))))
+        assertEquals(result.valid, false)
+      }
+    }
+  }
+}
+
+case class SchemaValue(value: Value)
+
+object SchemaValue {
+  def apply(json: String)(implicit parser: Parser): Either[String, SchemaValue] =
+    for {
+      value <- parser.parse(json)
+    } yield SchemaValue(value)
+}
+
+trait Handler {
+  def withKeyword(keyword: String, value: Value): Handler = this
+  def handle[R](calc: Calculator[R])(value: Value): R     = calc.invalid(NotHandled(this))
+}
+
+case class HandlerError(reason: String)    extends Observation
+case class NotHandled(handler: Handler)    extends Observation
+case object InvalidSchemaValue             extends Observation
+case class TypeMismatch2(expected: String) extends Observation
+
+trait Calculator[R] {
+  def valid(schema: SchemaValue): R
+  def invalid(observation: Observation): R
+  def prefix(prefix: Pointer, result: R): R
+//   def allOf(results: Seq[R]): R
+//   def anyOf(results: Seq[R]): R
+//   def oneOf(results: Seq[R]): R
+//   def not(result: R): R
+//   def ifThenElse(ifResult: R, thenResult: R, elseResult: R): R
+}
+case object RootHandler extends Handler {
+  override def withKeyword(keyword: String, value: Value): Handler = keyword match {
+    case _ => CoreHandler.withKeyword(keyword, value)
+  }
+}
+
+case object CoreHandler extends Handler {
+  override def withKeyword(keyword: String, value: Value): Handler = {
+    (keyword, value) match {
+      case ("type", StringValue("null"))    => NullHandler
+      case ("type", StringValue("boolean")) => BooleanHandler
+      case ("type", StringValue("string"))  => StringHandler
+      case ("type", StringValue("number"))  => NumberHandler
+      case ("type", StringValue("array"))   => ArrayHandler
+      case ("type", StringValue("object"))  => ObjectHandler
+      case _                                => ErroredHandler(s"unexpected keyword ${keyword} ${value}")
+    }
+  }
+  override def handle[R](calc: Calculator[R])(value: Value): R = calc.valid(SchemaValue(NullValue))
+}
+
+case class BoolHandler(value: Boolean) extends Handler {
+  override def withKeyword(keyword: String, value: Value): Handler = ???
+}
+case class ErroredHandler(reason: String) extends Handler {
+  override def withKeyword(keyword: String, value: Value): Handler = this
+  override def handle[R](calc: Calculator[R])(value: Value): R     = calc.invalid(HandlerError(reason))
+}
+
+case object NullHandler extends Handler {
+  override def handle[R](calc: Calculator[R])(value: Value): R = {
+    value match {
+      case NullValue => calc.valid(SchemaValue(NullValue))
+      case _         => calc.invalid(new TypeMismatch2("null"))
+    }
+  }
+}
+
+case object BooleanHandler extends Handler {}
+case object StringHandler  extends Handler {}
+case object NumberHandler  extends Handler {}
+case object ArrayHandler   extends Handler {}
+case object ObjectHandler extends Handler {
+  override def withKeyword(keyword: String, value: Value): Handler = (keyword, value) match {
+    case ("properties", ObjectValue(properties)) => PropertiesHandler(properties)
+    case _                                       => ErroredHandler(s"unexpected keyword ${keyword} ${value}")
+  }
+}
+
+case class PropertiesHandler(properties: Map[String, Value]) extends Handler {}
+
+object Processor {
+  def process[R](calc: Calculator[R])(schema: SchemaValue, value: Value): R = {
+    process(RootHandler, calc)(schema, value)
+  }
+
+  def process[R](handler: Handler, calc: Calculator[R])(schema: SchemaValue, value: Value): R = {
+    schema.value match {
+      case BoolValue(v) => BoolHandler(v).handle(calc)(value)
+      case ObjectValue(properties) =>
+        properties
+          .foldLeft((handler, calc.valid(schema))) { case ((handler, result), (keyword, v)) =>
+            val handler1 = handler.withKeyword(keyword, v)
+            val result1  = handler1.handle(calc)(value)
+            (handler1, result1)
+          }
+          ._2
+      case _ => calc.invalid(InvalidSchemaValue)
+    }
+
+  }
+}
+
+// ResultCalculator[ValidationResult]
+class ValidationCalculator extends Calculator[ValidationResult] {
+  override def valid(schema: SchemaValue): ValidationResult = ValidationValid
+  override def invalid(observation: Observation): ValidationResult = ValidationInvalid(
+    Seq(WithPointer(observation))
+  )
+  override def prefix(prefix: Pointer, result: ValidationResult): ValidationResult = result.prefix(prefix)
 }
