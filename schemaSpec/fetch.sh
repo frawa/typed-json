fa106548dd186b935d65fa2dcdacaa845db3eeca
Frank Wagner <agilecoderfrank@gmail.com>
2021-09-09 19:51:53 +0200

    downloaded schema spec

---
+++ schemaSpec/fetch.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+curl https://json-schema.org/draft/2020-12/schema -o schema.json
+curl https://json-schema.org/draft/2020-12/meta/core -o core.json
+curl https://json-schema.org/draft/2020-12/meta/applicator -o applicator.json
+curl https://json-schema.org/draft/2020-12/meta/validation -o validation.json
+curl https://json-schema.org/draft/2020-12/meta/unevaluated -o unevaluated.json
+curl https://json-schema.org/draft/2020-12/meta/meta-data -o meta-data.json
+curl https://json-schema.org/draft/2020-12/meta/format-annotation -o format-annotation.json
+curl https://json-schema.org/draft/2020-12/meta/content -o content.json
