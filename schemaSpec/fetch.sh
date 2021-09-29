a31e55fbf862f5d2f61c87705a03a644cd263d1b
Frank Wagner <agilecoderfrank@gmail.com>
2021-09-29 23:00:24 +0200

    schema for output

--- schemaSpec/fetch.sh
+++ schemaSpec/fetch.sh
@@ -8,3 +8,6 @@ curl https://json-schema.org/draft/2020-12/meta/unevaluated -o unevaluated.json
 curl https://json-schema.org/draft/2020-12/meta/meta-data -o meta-data.json
 curl https://json-schema.org/draft/2020-12/meta/format-annotation -o format-annotation.json
 curl https://json-schema.org/draft/2020-12/meta/content -o content.json
+
+mkdir -p output
+curl https://json-schema.org/draft/2020-12/output/schema -o output/schema.json
