a193bfc1f5535ca52b7169ae7d450f554ba0ee2e
Frank Wagner <agilecoderfrank@gmail.com>
2021-11-01 19:51:01 +0100

    simplify a bit

--- validator/src/test/scala/frawa/typedjson/schema/PointerTest.scala
+++ validator/src/test/scala/frawa/typedjson/schema/PointerTest.scala
@@ -9,94 +9,94 @@ import frawa.typedjson.parser.ObjectValue
 // see https://datatracker.ietf.org/doc/html/rfc6901
 
 class PointerTest extends FunSuite {
+  import Pointer._
 
   test("empty") {
-    val pointer = Pointer.empty
-    assertEquals(pointer.toString, "")
+    assertEquals(empty.toString, "")
   }
 
   test("array") {
-    assertEquals((Pointer.empty / 13).toString, "/13")
+    assertEquals((empty / 13).toString, "/13")
   }
 
   test("field") {
-    assertEquals((Pointer.empty / "toto").toString, "/toto")
+    assertEquals((empty / "toto").toString, "/toto")
   }
 
   test("deep field") {
-    assertEquals((Pointer.empty / "$defs" / "gnu").toString, "/$defs/gnu")
+    assertEquals((empty / "$defs" / "gnu").toString, "/$defs/gnu")
   }
 
   test("field with /") {
-    assertEquals((Pointer.empty / "toto/titi").toString, "/toto~1titi")
+    assertEquals((empty / "toto/titi").toString, "/toto~1titi")
   }
 
   test("field with ~") {
-    assertEquals((Pointer.empty / "toto~titi").toString, "/toto~0titi")
+    assertEquals((empty / "toto~titi").toString, "/toto~0titi")
   }
 
   test("append") {
-    val pointer1 = Pointer.empty / "toto"
-    val pointer2 = Pointer.empty / 13
+    val pointer1 = empty / "toto"
+    val pointer2 = empty / 13
     assertEquals((pointer1 / pointer2).toString, "/toto/13")
   }
 
   test("parse") {
-    assertEquals(Pointer.parse("/toto/titi"), Pointer.empty / "toto" / "titi")
-    assertEquals(Pointer.parse("/$defs/gnu"), Pointer.empty / "$defs" / "gnu")
-    assertEquals(Pointer.parse("/toto~1titi"), Pointer.empty / "toto/titi")
-    assertEquals(Pointer.parse("/toto~0titi"), Pointer.empty / "toto~titi")
+    assertEquals(parse("/toto/titi"), empty / "toto" / "titi")
+    assertEquals(parse("/$defs/gnu"), empty / "$defs" / "gnu")
+    assertEquals(parse("/toto~1titi"), empty / "toto/titi")
+    assertEquals(parse("/toto~0titi"), empty / "toto~titi")
   }
 
   test("get root") {
     val value = NumberValue(13)
-    assertEquals(Pointer.empty(value), Some(value))
+    assertEquals(empty(value), Some(value))
   }
 
   test("get array item") {
     val value = ArrayValue(Seq(NumberValue(13), NumberValue(14)))
-    assertEquals((Pointer.empty / 1)(value), Some(NumberValue(14)))
-    assertEquals((Pointer.empty / 13)(value), None)
+    assertEquals((empty / 1)(value), Some(NumberValue(14)))
+    assertEquals((empty / 13)(value), None)
   }
 
   test("object field item") {
     val value = ObjectValue(Map("foo" -> NumberValue(13), "gnu" -> NumberValue(14)))
-    assertEquals((Pointer.empty / "foo")(value), Some(NumberValue(13)))
-    assertEquals((Pointer.empty / "bar")(value), None)
+    assertEquals((empty / "foo")(value), Some(NumberValue(13)))
+    assertEquals((empty / "bar")(value), None)
   }
 
   test("deep object field item") {
     val value = ObjectValue(
       Map("foo" -> ObjectValue(Map("gnu" -> NumberValue(14))), "gnu" -> NumberValue(15))
     )
-    assertEquals((Pointer.empty / "foo" / "gnu")(value), Some(NumberValue(14)))
-    assertEquals((Pointer.empty / "bar")(value), None)
+    assertEquals((empty / "foo" / "gnu")(value), Some(NumberValue(14)))
+    assertEquals((empty / "bar")(value), None)
   }
 
   test("parsing roundtrip") {
-    assertEquals(Pointer.parse(Pointer.empty.toString), Pointer.empty)
-    val p1 = Pointer.empty / "foo"
-    assertEquals(Pointer.parse(p1.toString), p1)
-    val p2 = Pointer.empty / "foo" / 13
-    assertEquals(Pointer.parse(p2.toString), p2)
+    assertEquals(parse(empty.toString), empty)
+    val p1 = empty / "foo"
+    assertEquals(parse(p1.toString), p1)
+    val p2 = empty / "foo" / 13
+    assertEquals(parse(p2.toString), p2)
 
     val s1 = ""
-    assertEquals(Pointer.parse(s1).toString, s1)
+    assertEquals(parse(s1).toString, s1)
     val s2 = "/foo"
-    assertEquals(Pointer.parse(s2).toString, s2)
+    assertEquals(parse(s2).toString, s2)
     val s3 = "/foo/13"
-    assertEquals(Pointer.parse(s3).toString, s3)
+    assertEquals(parse(s3).toString, s3)
   }
 
   test("get parsed array item") {
     val value = ArrayValue(Seq(NumberValue(13), NumberValue(14)))
-    assertEquals(Pointer.parse("/1")(value), Some(NumberValue(14)))
-    assertEquals(Pointer.parse("/13")(value), None)
+    assertEquals(parse("/1")(value), Some(NumberValue(14)))
+    assertEquals(parse("/13")(value), None)
   }
 
   test("parsed object field item") {
     val value = ObjectValue(Map("foo" -> NumberValue(13), "14" -> NumberValue(14)))
-    assertEquals(Pointer.parse("/foo")(value), Some(NumberValue(13)))
-    assertEquals(Pointer.parse("/14")(value), Some(NumberValue(14)))
+    assertEquals(parse("/foo")(value), Some(NumberValue(13)))
+    assertEquals(parse("/14")(value), Some(NumberValue(14)))
   }
 }
