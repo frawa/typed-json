4618309a96ae997fe781c756a311166caae822f1
Frank Wagner <agilecoderfrank@gmail.com>
2021-12-09 00:17:25 +0100

    first json editor

--- sample-editor/src/app.ts
+++ sample-editor/src/app.ts
@@ -1,1 +1,36 @@
-console.log('Hello world!');
+import { EditorState, EditorView, basicSetup } from "@codemirror/next/basic-setup"
+import { json } from "@codemirror/next/lang-json"
+import { autocompletion } from "@codemirror/next/autocomplete"
+
+// see https://codemirror.net/6/docs/ref/
+
+const state = EditorState.create({
+    doc: `{
+  "hello": "world"
+}`,
+    extensions: [
+        basicSetup,
+        json(),
+        autocompletion({
+            activateOnTyping: false,
+            defaultKeymap: true,
+            override: [context => {
+                return {
+                    from: context.pos,
+                    options: [{
+                        label: "a suggestion"
+                    }]
+                }
+            }]
+        }),
+        // keymap.of(completionKeymap)
+    ],
+});
+
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+(window as any).view =
+    new EditorView({
+        state,
+        parent: document.querySelector("#editor") || undefined
+    })
