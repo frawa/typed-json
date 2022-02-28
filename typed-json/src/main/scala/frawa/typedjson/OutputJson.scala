9411100822448468b5310d1d156fed2b996bc429
Frank Wagner <agilecoderfrank@gmail.com>
2022-02-28 22:11:33 +0100

    start basic output

--- typed-json/src/main/scala/frawa/typedjson/OutputJson.scala
+++ typed-json/src/main/scala/frawa/typedjson/OutputJson.scala
@@ -2,10 +2,35 @@ package frawa.typedjson
 
 import frawa.typedjson.parser.Value
 import frawa.typedjson.parser.Value._
+import frawa.typedjson.validation.ValidationError
 
 object OutputJson {
 
   def flag(validation: TypedJson.Validation): Value = {
     ObjectValue(Map("valid" -> BoolValue(validation.valid)))
+  }
+
+  def basic(validation: TypedJson.Validation): Value = {
+    val errors = if (validation.output.errors.isEmpty) {
+      Map()
+    } else {
+      Map("errors" -> ArrayValue(validation.output.errors.map(toJson)))
+    }
+    ObjectValue(Map("valid" -> BoolValue(validation.valid)) ++ errors)
+  }
+
+  private def toJson(error: TypedJson.Error): Value = {
+    ObjectValue(
+      Map(
+        "error" -> StringValue(toMessage(error.error)),
+        // "keywordLocation"         -> StringValue(""),
+        // "absoluteKeywordLocation" -> StringValue(""),
+        "instanceLocation" -> StringValue(error.pointer.toString)
+      )
+    )
+  }
+
+  private def toMessage(error: ValidationError): String = {
+    error.toString
   }
 }
--- typed-json/src/test/scala/frawa/typedjson/OutputJsonTest.scala
+++ typed-json/src/test/scala/frawa/typedjson/OutputJsonTest.scala
@@ -3,6 +3,9 @@ package frawa.typedjson
 import munit.FunSuite
 
 import frawa.typedjson.parser.Value._
+import frawa.typedjson.validation.ValidationResult
+import frawa.typedjson.keywords.Result
+import frawa.typedjson.validation.FalseSchemaReason
 
 class OutputJsonTest extends FunSuite {
 
@@ -17,6 +20,36 @@ class OutputJsonTest extends FunSuite {
     assertEquals(
       OutputJson.flag(TypedJson.Validation(false, TypedJson.Output(Nil))),
       ObjectValue(Map("valid" -> BoolValue(false)))
+    )
+  }
+
+  test("basic valid") {
+    assertEquals(
+      OutputJson.basic(TypedJson.Validation(true, TypedJson.Output(Nil))),
+      ObjectValue(Map("valid" -> BoolValue(true)))
+    )
+  }
+
+  test("basic errors") {
+    assertEquals(
+      OutputJson.basic(
+        TypedJson.Validation(false, TypedJson.Output(Result.valid(ValidationResult.invalid(FalseSchemaReason()))))
+      ),
+      ObjectValue(
+        Map(
+          "valid" -> BoolValue(false),
+          "errors" -> ArrayValue(
+            Seq(
+              ObjectValue(
+                properties = Map(
+                  "error"            -> StringValue("FalseSchemaReason()"),
+                  "instanceLocation" -> StringValue("")
+                )
+              )
+            )
+          )
+        )
+      )
     )
   }
 
