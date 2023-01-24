14118056f7c8ac763b7c5b41d26f210b0fbc4092
Frank Wagner <agilecoderfrank@gmail.com>
2023-01-24 21:22:22 +0100

    rename to Verify

--- typed-json/src/main/scala/frawa/typedjson/eval/Verify.scala
+++ typed-json/src/main/scala/frawa/typedjson/eval/Verify.scala
@@ -8,7 +8,7 @@ import frawa.typedjson.parser.Value._
 import frawa.typedjson.pointer.Pointer
 import frawa.typedjson.validation.FalseSchemaReason
 
-class Validate[O: OutputOps]:
+class Verify[O: OutputOps]:
   import Eval.Fun
 
   val nullTypeMismatch    = TypeMismatch[NullValue.type]("null")
@@ -20,15 +20,15 @@ class Validate[O: OutputOps]:
 
   private val ops = summon[OutputOps[O]]
 
-  def validateType[T <: Value](error: TypeMismatch[T])(using TypeTest[Value, T]): Fun[O] = value =>
+  def verifyType[T <: Value](error: TypeMismatch[T])(using TypeTest[Value, T]): Fun[O] = value =>
     value match
       case _: T => ops.valid
       case _    => ops.invalid(error, Pointer.empty)
 
-  def validateTrivial(valid: Boolean): Fun[O] = value =>
+  def verifyTrivial(valid: Boolean): Fun[O] = value =>
     if valid then ops.valid
     else ops.invalid(FalseSchemaReason(), Pointer.empty)
 
-  def validateNot(f: Fun[O]): Fun[O]         = ???
-  def validateUnion(fs: Seq[Fun[O]]): Fun[O] = ???
-  def validateAll(fs: Seq[Fun[O]]): Fun[O]   = value => ops.all(fs.map(_(value)))
+  def verifyNot(f: Fun[O]): Fun[O]         = ???
+  def verifyUnion(fs: Seq[Fun[O]]): Fun[O] = ???
+  def verifyAll(fs: Seq[Fun[O]]): Fun[O] = value => ops.all(fs.map(_(value)))
