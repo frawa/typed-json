4a08b5d6cb1ae759f2941445ee6e3a4d0e81a63c
Frank Wagner <agilecoderfrank@gmail.com>
2023-01-24 21:13:23 +0100

    introduce output with errors

--- typed-json/src/test/scala/frawa/typedjson/eval/EvalTest.scala
+++ typed-json/src/test/scala/frawa/typedjson/eval/EvalTest.scala
@@ -16,10 +16,11 @@ import scala.reflect.TypeTest
 class EvalTest extends FunSuite:
   import Util.{_, given}
 
-  val eval             = Eval[MyR, MyO]
-  given Eval[MyR, MyO] = eval
+  val eval  = Eval[MyR, MyO]
+  val eval2 = Eval[MyResult, MyOutput]
 
   test("null") {
+    given Eval[MyR, MyO] = eval
     withCompiledSchema(nullSchema) { fun =>
       assertEquals(fun(NullValue), MyO(true))
       assertEquals(fun(BoolValue(true)), MyO(false))
@@ -27,9 +28,24 @@ class EvalTest extends FunSuite:
   }
 
   test("true") {
+    given Eval[MyR, MyO] = eval
     withCompiledSchema(trueSchema) { fun =>
       assertEquals(fun(BoolValue(true)), MyO(true))
       assertEquals(fun(NullValue), MyO(true))
+    }
+  }
+
+  test("null with errors") {
+    given Eval[MyResult, MyOutput] = eval2
+    withCompiledSchema(nullSchema) { fun =>
+      assertEquals(fun(NullValue), MyOutput(true, Seq()))
+      assertEquals(
+        fun(BoolValue(true)),
+        MyOutput(
+          false,
+          Seq(TypeMismatch("null"))
+        )
+      )
     }
   }
 
@@ -73,3 +89,26 @@ object Util:
       def not: MyO         = ???
       def isValid: Boolean = ???
     //   def combine(o2: MyO): MyO = all(Seq(o, o2))
+
+  type MyResult[O] = MyR[O]
+  case class MyOutput(valid: Boolean, errors: Seq[ValidationError])
+
+  // given TheResultMonad[MyResult] with
+  //   def unit[A](a: A): MyResult[A]                                      = a
+  //   def flatMap[A, B](a: MyResult[A])(f: A => MyResult[B]): MyResult[B] = f(a)
+  //   def output[O](result: MyResult[O]): O                               = result
+
+  given OutputOps[MyOutput] with
+    def valid: MyOutput                                                     = MyOutput(true, Seq())
+    def valid(annotation: ValidationAnnotation, pointer: Pointer): MyOutput = MyOutput(true, Seq())
+    def invalid(error: ValidationError, pointer: Pointer): MyOutput         = MyOutput(false, Seq(error))
+    def invalid(problems: SchemaProblems): MyOutput                         = MyOutput(false, Seq())
+
+    def all(os: Seq[MyOutput]): MyOutput = MyOutput(os.forall(_.valid), os.flatMap(_.errors))
+    def any(os: Seq[MyOutput]): MyOutput = ???
+    def one(os: Seq[MyOutput]): MyOutput = ???
+    def contains(os: Seq[MyOutput], min: Option[Int], max: Option[Int], pointer: Pointer): MyOutput = ???
+
+    extension (o: MyOutput)
+      def not: MyOutput    = ???
+      def isValid: Boolean = ???
