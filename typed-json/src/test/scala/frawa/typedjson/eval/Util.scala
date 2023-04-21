b589eda90e42c35ce5aa2bd1296b500b7a03df39
Frank Wagner <agilecoderfrank@gmail.com>
2023-04-21 21:30:11 +0200

    fix cleanup during Intellj-commit (WTF?)

--- typed-json/src/test/scala/frawa/typedjson/eval/Util.scala
+++ typed-json/src/test/scala/frawa/typedjson/eval/Util.scala
@@ -3,20 +3,21 @@ package frawa.typedjson.eval
 import frawa.typedjson.eval.MyState.MyR
 import frawa.typedjson.keywords.*
 import frawa.typedjson.parser.Value
-import frawa.typedjson.testutil.TestUtil.*
+import frawa.typedjson.testutil.TestUtil.{*, given}
 import frawa.typedjson.util.UriUtil
 import frawa.typedjson.validation.CannotResolve
 
 import java.net.URI
 
 object Util:
-  val vocabularyForTest = dialect(Seq(Vocabulary.coreId, Vocabulary.validationId, Vocabulary.applicatorId))
-  // val vocabularyForTest = dialect(Seq(Vocabulary.coreId, Vocabulary.validationId))
+  val vocabularyForTest: Option[Vocabulary] = dialect(
+    Seq(Vocabulary.coreId, Vocabulary.validationId, Vocabulary.applicatorId)
+  )
 
   def withKeywords(schema: SchemaValue, lazyResolver: Option[LoadedSchemasResolver.LazyResolver] = None)(
-    f: Keywords => Unit
+      f: Keywords => Unit
   ): Unit =
-  // TODO avoid lazyResolver
+    // TODO avoid lazyResolver
     Keywords(schema, vocabularyForTest, lazyResolver)
       .fold(
         errors => throw new IllegalArgumentException(s"no keywords: $errors"),
@@ -26,21 +27,19 @@ object Util:
   type AssertingFun[R[_], O] = SchemaResolver ?=> R[Value => O] => Unit
 
   def withCompiledSchema[R[_], O](
-                                   schema: String,
-                                   lazyResolver: Option[LoadedSchemasResolver.LazyResolver] = None
-                                 )(using eval: Eval[R, O])(using TheResultMonad[R, O])(f: AssertingFun[R, O]): Unit =
+      schema: String,
+      lazyResolver: Option[LoadedSchemasResolver.LazyResolver] = None
+  )(using eval: Eval[R, O])(using TheResultMonad[R, O])(f: AssertingFun[R, O]): Unit =
     withSchema(schema) { schema =>
       withKeywords(schema, lazyResolver) { keywords =>
-        val c = eval.compile(keywords)
-        val fun = eval.fun(c)
-
+        val c                = eval.compile(keywords)
+        val fun              = eval.fun(c)
         given SchemaResolver = LoadedSchemasResolver(schema, lazyResolver)
-
         f(fun)
       }
     }
 
-  given[O: OutputOps]: TheResultMonad[MyR[O], O] with
+  given [O: OutputOps]: TheResultMonad[MyR[O], O] with
     def unit[A](a: A): MyR[O][A] = s => (a, s)
 
     def bind[A, B](a: MyR[O][A])(f: A => MyR[O][B]): MyR[O][B] = s =>
@@ -48,11 +47,11 @@ object Util:
       f(a2)(s2.copy(count = s2.count + 1))
 
     def resolve(ref: String, base: URI, scope: DynamicScope)(using
-                                                             eval: Eval[MyR[O], O]
+        eval: Eval[MyR[O], O]
     ): MyR[O][Eval.Fun[O]] =
       MyState.resolve(ref, base, scope)
 
     def resolveDynamic(ref: String, base: URI, scope: DynamicScope)(using
-                                                                    eval: Eval[MyR[O], O]
+        eval: Eval[MyR[O], O]
     ): MyR[O][Eval.Fun[O]] =
       MyState.resolveDynamic(ref, base, scope)
