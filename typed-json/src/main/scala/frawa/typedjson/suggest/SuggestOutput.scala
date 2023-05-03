1d2e74df05a0cbee5060f2b2698fa40e2922e627
Frank Wagner <agilecoderfrank@gmail.com>
2023-05-03 22:52:08 +0200

    compile fix

--- typed-json/src/main/scala/frawa/typedjson/suggest/SuggestOutput.scala
+++ typed-json/src/main/scala/frawa/typedjson/suggest/SuggestOutput.scala
@@ -25,23 +25,23 @@ import frawa.typedjson.validation.NotOneOf
 import frawa.typedjson.validation.NotInvalid
 import frawa.typedjson.keywords.Evaluated
 import frawa.typedjson.keywords.Keyword
-import frawa.typedjson.output.BasicOutput
+import frawa.typedjson.output.SimpleOutput
 import frawa.typedjson.output.OutputOps
 import frawa.typedjson.suggest.Suggest
 
 case class SuggestOutput(
-    basic: BasicOutput,
+    simple: SimpleOutput,
     keywords: Seq[Keyword] = Seq.empty
 )
 
 object SuggestOutput:
-  import BasicOutput.given
+  import SimpleOutput.given
 
   def outputOps(at: Pointer): OutputOps[SuggestOutput] =
     SuggestOutputOps(at)
 
   private class SuggestOutputOps(private val at: Pointer) extends OutputOps[SuggestOutput]:
-    private val bops = summon[OutputOps[BasicOutput]]
+    private val bops = summon[OutputOps[SimpleOutput]]
     private val isAt = Suggest.isAt(at)
 
     def valid(pointer: Pointer): SuggestOutput =
@@ -52,16 +52,16 @@ object SuggestOutput:
 
     def all(os: Seq[SuggestOutput], pointer: Pointer): SuggestOutput =
       SuggestOutput(
-        bops.all(os.map(_.basic), pointer),
+        bops.all(os.map(_.simple), pointer),
         os.flatMap(_.keywords)
       )
 
     extension (o: SuggestOutput)
-      def not(pointer: Pointer): SuggestOutput = o.copy(basic = o.basic.not(pointer))
-      def isValid: Boolean                     = o.basic.valid
+      def not(pointer: Pointer): SuggestOutput = o.copy(simple = o.simple.not(pointer))
+      def isValid: Boolean                     = o.simple.valid
       def withAnnotations(annotations: Seq[Evaluated]): SuggestOutput =
-        o.copy(basic = o.basic.withAnnotations(annotations))
-      def getAnnotations(): Seq[Evaluated] = o.basic.annotations
+        o.copy(simple = o.simple.withAnnotations(annotations))
+      def getAnnotations(): Seq[Evaluated] = o.simple.annotations
       def forKeyword(k: Keyword): SuggestOutput =
-        if isAt(o.basic.pointer) then o.copy(keywords = o.keywords :+ k)
+        if isAt(o.simple.pointer) then o.copy(keywords = o.keywords :+ k)
         else o
