e9388acebd20e1dc2e99417d8f26d1e6a29436d2
Frank Wagner <agilecoderfrank@gmail.com>
2023-05-06 00:33:00 +0200

    fix formatting

--- parser/src/main/scala/frawa/typedjson/parser/Parser.scala
+++ parser/src/main/scala/frawa/typedjson/parser/Parser.scala
@@ -25,32 +25,32 @@ object Parser:
 
 sealed trait Value
 object Value:
-  case class NumberValue(value: BigDecimal)              extends Value
-  case class BoolValue(value: Boolean)                   extends Value
-  case object NullValue                                  extends Value
-  case class StringValue(value: String)                  extends Value
-  case class ArrayValue(items: Seq[Value])               extends Value
+  case class NumberValue(value: BigDecimal) extends Value
+  case class BoolValue(value: Boolean)      extends Value
+  case object NullValue                     extends Value
+  case class StringValue(value: String)     extends Value
+  case class ArrayValue(items: Seq[Value])  extends Value
 
   case class ObjectValue(properties: Map[String, Value]) extends Value
 
   def asString(value: Value): Option[String] = value match
     case StringValue(v) => Some(v)
-    case _ => None
+    case _              => None
 
   def asBool(value: Value): Option[Boolean] = value match
     case BoolValue(v) => Some(v)
-    case _ => None
+    case _            => None
 
   def asNumber(value: Value): Option[BigDecimal] = value match
     case NumberValue(v) => Some(v)
-    case _ => None
+    case _              => None
 
   def asObject(value: Value): Option[Map[String, Value]] = value match
     case ObjectValue(v) => Some(v)
-    case _ => None
+    case _              => None
 
   def asStrings(values: Seq[Value]): Seq[String] = values.flatMap(asString)
 
   def asArray(value: Value): Option[Seq[Value]] = value match
     case ArrayValue(v) => Some(v)
-    case _ => None
+    case _             => None
