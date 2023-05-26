7e7df67b1993999d6cac94e017a188e85b325373
Frank Wagner <agilecoderfrank@gmail.com>
2023-05-26 08:51:25 +0200

    cleanup test

--- parser/src/test/scala/frawa/typedjson/parser/OffsetParserTest.scala
+++ parser/src/test/scala/frawa/typedjson/parser/OffsetParserTest.scala
@@ -34,13 +34,6 @@ class OffsetParserTest extends FunSuite:
     assertEquals(contextAt(v)(13), NewValue(Pointer.empty, Offset(13, 13)))
   }
 
-  // test("pointerAt is empty on number") {
-  //   val v = NumberValue(Offset(0, 2), 13)
-  //   assertEquals(pointerAt(v)(0), Pointer.empty)
-  //   assertEquals(pointerAt(v)(1), Pointer.empty)
-  //   assertEquals(pointerAt(v)(13), Pointer.empty)
-  // }
-
   test("contextAt around bool") {
     val v = BoolValue(Offset(1, 5), value = true)
     assertEquals(contextAt(v)(0), NewValue(Pointer.empty, Offset(0, 0)))
@@ -49,22 +42,6 @@ class OffsetParserTest extends FunSuite:
     assertEquals(contextAt(v)(5), NewValue(Pointer.empty, Offset(5, 5)))
     assertEquals(contextAt(v)(13), NewValue(Pointer.empty, Offset(13, 13)))
   }
-
-  // test("pointerAt is empty on bool") {
-  //   val v = BoolValue(Offset(0, 4), value = true)
-  //   assertEquals(pointerAt(v)(0), Pointer.empty)
-  //   assertEquals(pointerAt(v)(2), Pointer.empty)
-  // }
-
-  // test("pointerAt is empty on null") {
-  //   val v = NullValue(Offset(0, 4))
-  //   assertEquals(pointerAt(v)(3), Pointer.empty)
-  // }
-
-  // test("pointerAt is empty on string") {
-  //   val v = StringValue(Offset(0, 5), "foo")
-  //   assertEquals(pointerAt(v)(4), Pointer.empty)
-  // }
 
   test("contextAt around array") {
     // [13,14]
@@ -79,16 +56,6 @@ class OffsetParserTest extends FunSuite:
     assertEquals(contextAt(v)(7), NewValue(Pointer.empty, Offset(7, 7)))
     assertEquals(contextAt(v)(13), NewValue(Pointer.empty, Offset(13, 13)))
   }
-
-  // test("pointerAt array") {
-  //   // [13,14]
-  //   val v = ArrayValue(Offset(0, 7), Seq(NumberValue(Offset(1, 3), 13), NumberValue(Offset(4, 6), 14)))
-  //   assertEquals(pointerAt(v)(0), Pointer.empty)
-  //   assertEquals(pointerAt(v)(1), Pointer.empty / 0)
-  //   assertEquals(pointerAt(v)(4), Pointer.empty / 1)
-  //   assertEquals(pointerAt(v)(7), Pointer.empty / 1)
-  //   assertEquals(pointerAt(v)(8), Pointer.empty)
-  // }
 
   test("contextAt around nested array") {
     // [1,[2]]
@@ -106,20 +73,6 @@ class OffsetParserTest extends FunSuite:
     assertEquals(contextAt(v)(7), NewValue(Pointer.empty, Offset(7, 7)))
     assertEquals(contextAt(v)(13), NewValue(Pointer.empty, Offset(13, 13)))
   }
-
-  // test("pointerAt nested array") {
-  //   // [1,[2]]
-  //   val v = ArrayValue(
-  //     Offset(0, 8),
-  //     Seq(NumberValue(Offset(1, 2), 1), ArrayValue(Offset(3, 7), Seq(NumberValue(Offset(4, 5), 2))))
-  //   )
-  //   assertEquals(pointerAt(v)(0), Pointer.empty)
-  //   assertEquals(pointerAt(v)(1), Pointer.empty / 0)
-  //   assertEquals(pointerAt(v)(3), Pointer.empty / 1)
-  //   assertEquals(pointerAt(v)(4), Pointer.empty / 1 / 0)
-  //   assertEquals(pointerAt(v)(7), Pointer.empty / 1)
-  //   assertEquals(pointerAt(v)(8), Pointer.empty)
-  // }
 
   test("contextAt around object") {
     // {"toto": 13}
@@ -142,24 +95,6 @@ class OffsetParserTest extends FunSuite:
     assertEquals(contextAt(v)(12), NewValue(Pointer.empty, Offset(12, 12)))
     assertEquals(contextAt(v)(13), NewValue(Pointer.empty, Offset(13, 13)))
   }
-
-  // test("pointerAt object") {
-  //   // {"toto": 13}
-  //   val v = ObjectValue(
-  //     Offset(0, 12),
-  //     Map(StringValue(Offset(1, 7), "toto") -> NumberValue(Offset(9, 11), 13))
-  //   )
-  //   assertEquals(pointerAt(v)(0), Pointer.empty)
-  //   assertEquals(pointerAt(v)(1), (Pointer.empty / "toto").insideKey)
-  //   assertEquals(pointerAt(v)(6), (Pointer.empty / "toto").insideKey)
-  //   assertEquals(pointerAt(v)(7), Pointer.empty / "toto")
-  //   assertEquals(pointerAt(v)(8), Pointer.empty / "toto")
-  //   assertEquals(pointerAt(v)(9), Pointer.empty / "toto")
-  //   assertEquals(pointerAt(v)(10), Pointer.empty / "toto")
-  //   assertEquals(pointerAt(v)(11), Pointer.empty.insideKey)
-  //   assertEquals(pointerAt(v)(12), Pointer.empty)
-  //   assertEquals(pointerAt(v)(13), Pointer.empty)
-  // }
 
   test("contextAt around nexted object") {
     // {"toto": 1, "titi": {"foo": true}}
@@ -217,42 +152,3 @@ class OffsetParserTest extends FunSuite:
   // - arrays
   // - object keys
   // - object values
-
-  // test("pointerAt nested object") {
-  //   // {"toto": 1, "titi": {"foo": true}}
-  //   val v = ObjectValue(
-  //     Offset(0, 34),
-  //     Map(
-  //       StringValue(Offset(1, 7), "toto") -> NumberValue(Offset(9, 10), 1),
-  //       StringValue(Offset(12, 18), "titi") -> ObjectValue(
-  //         Offset(20, 33),
-  //         Map(
-  //           StringValue(Offset(21, 26), "foo") -> BoolValue(Offset(28, 32), true)
-  //         )
-  //       )
-  //     )
-  //   )
-  //   assertEquals(pointerAt(v)(0), Pointer.empty)
-  //   assertEquals(pointerAt(v)(1), (Pointer.empty / "toto").insideKey)
-  //   assertEquals(pointerAt(v)(6), (Pointer.empty / "toto").insideKey)
-  //   assertEquals(pointerAt(v)(7), Pointer.empty / "toto")
-  //   assertEquals(pointerAt(v)(8), Pointer.empty / "toto")
-  //   assertEquals(pointerAt(v)(9), Pointer.empty / "toto")
-  //   assertEquals(pointerAt(v)(10), Pointer.empty.insideKey)
-  //   assertEquals(pointerAt(v)(12), (Pointer.empty / "titi").insideKey)
-  //   assertEquals(pointerAt(v)(17), (Pointer.empty / "titi").insideKey)
-  //   assertEquals(pointerAt(v)(19), Pointer.empty / "titi")
-  //   assertEquals(pointerAt(v)(20), Pointer.empty / "titi")
-  //   assertEquals(pointerAt(v)(21), (Pointer.empty / "titi" / "foo").insideKey)
-  //   assertEquals(pointerAt(v)(25), (Pointer.empty / "titi" / "foo").insideKey)
-  //   assertEquals(pointerAt(v)(26), Pointer.empty / "titi" / "foo")
-  //   assertEquals(pointerAt(v)(28), Pointer.empty / "titi" / "foo")
-  //   assertEquals(pointerAt(v)(31), Pointer.empty / "titi" / "foo")
-  //   assertEquals(pointerAt(v)(32), (Pointer.empty / "titi").insideKey)
-  //   assertEquals(pointerAt(v)(33), Pointer.empty.insideKey)
-  //   assertEquals(pointerAt(v)(34), Pointer.empty)
-  //   assertEquals(pointerAt(v)(35), Pointer.empty)
-  // }
-
-  // TODO
-  // test("roundtrip pointerAt/offssetAt".ignore) {}
